3a523350b2dda9e2b932dd27ab4a3b45bd46cc45
mahdi <mahmodimahdi26@gmail.com>
2023-12-12 19:46:51 +0330

    feat: create Hard Disk hardware test

---
+++ tests/Hardware/Storage/HardDiskTests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Hardware\Storage;
+
+use Mahmodi\ComputerSimulator\Hardware\Storage\HardDisk;
+use PHPUnit\Framework\TestCase;
+
+class HardDiskTests extends TestCase
+{
+    
+}
---
+++ tests/Hardware/Storage/HasAssetBuilderTraitTests.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Hardware\Storage;
+
+use PHPUnit\Framework\TestCase;
+
+class HasAssetBuilderTraitTests extends TestCase
+{
+ 
+}
