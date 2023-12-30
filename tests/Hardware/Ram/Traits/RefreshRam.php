e06d76625c465724de737df0c3cb466176c49ff4
mahdi <mahmodimahdi26@gmail.com>
2023-12-30 20:09:08 +0330

    feat: create RefreshRam trait for TDD test of Ram hardware"

---
+++ tests/Hardware/Ram/Traits/RefreshRam.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Hardware\Ram\Traits;
+
+trait RefreshRam
+{
+    /**
+     * The new Ram instance will store on it
+     *
+     * @var Ram|null
+     */
+    protected static ?Ram $ram = null;
+
+    /**
+     * Create new instance of Ram and then store in the property
+     * before run test
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        self::$ram = new Ram();
+    }
+
+    /**
+     * Destroy created ram instance after executed test
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        self::$ram = null;
+    }
+}
